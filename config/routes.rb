3ea219948c5a26430765480a2b2f5df766b49572
Arpith Siromoney <arpith@feedreader.co>
2017-05-11 12:42:05 +0530

    devise & administrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :users
+    root to: "users#index"
+    resources :positions
+    root to: "positions#index"
+  end
+
+  devise_for :users
+
+  get 'directory/index'
+
+  root 'directory#index'
 end
