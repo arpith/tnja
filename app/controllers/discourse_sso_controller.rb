5a1c4237a5660fbe45e51da36348f89b4eedb119
Arpith Siromoney <arpith@feedreader.co>
2017-05-17 10:53:04 +0530

    add full_name field to user model

--- app/controllers/discourse_sso_controller.rb
+++ app/controllers/discourse_sso_controller.rb
@@ -7,7 +7,7 @@ class DiscourseSsoController < ApplicationController
     secret = ENV['SSO_SECRET']
     sso = SingleSignOn.parse(request.query_string, secret)
     sso.email = current_user.email # from devise
-    sso.name = current_user.full_name # this is a custom method on the User class
+    sso.name = current_user.full_name # from model
     sso.username = current_user.email # from devise
     sso.external_id = current_user.id # from devise
     sso.sso_secret = secret
