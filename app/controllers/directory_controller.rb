54dfa2f23cdb2bcf3602610475cf6f7fe36918c4
Arpith Siromoney <arpith@feedreader.co>
2017-05-19 20:38:05 +0530

    fix directory view / controller

--- app/controllers/directory_controller.rb
+++ app/controllers/directory_controller.rb
@@ -3,8 +3,6 @@ class DirectoryController < ApplicationController
     @categories = Hash.new
     Category.all.each do |category|
       @categories[category.name] = category.users.select(:rank, :full_name)
-      @categories["a"] = "b"
     end
-    puts @categories
   end
 end
