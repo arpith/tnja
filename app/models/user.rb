7b59a4510a8897999dc7442e47790a76d89d4797
Arpith Siromoney <arpith@feedreader.co>
2017-07-13 12:45:01 +0530

    user model: make position & category optional

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  belongs_to :position
-  belongs_to :category
+  belongs_to :position, optional: true
+  belongs_to :category, optional: true
   has_many :payments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
